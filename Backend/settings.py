757869991a486a5f1b45ca93e486096852efded9
AndresGiron <andres.giron.perez@correounivalle.edu.co>
2023-10-31 16:52:06 -0500

    Redesplegando

--- Backend/settings.py
+++ Backend/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Agrega aquí los dominios permitidos
+    "http://localhost:3000", "https://inmunodetecta.me" # Agrega aquí los dominios permitidos
 ]
 
 ROOT_URLCONF = 'Backend.urls'
